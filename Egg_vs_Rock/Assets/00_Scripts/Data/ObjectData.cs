cd2d729dbc23dc6a49513d5b5558ede76f20ec8c
Hongshinyoung <gtd4095@naver.com>
2025-06-18 15:55:06 +0900

    Feat : Sheet To Json

---
+++ Egg_vs_Rock/Assets/00_Scripts/Data/ObjectData.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectData
+{
+    public List<EggData> EggDataList;
+    public List<RockData> RockDataList;
+}
+
+[Serializable]
+public class EggData
+{
+    public string Id;
+    public int Hp;
+    public float Damage;
+}
+
+[Serializable]
+public class RockData
+{
+    public string Id;
+    public int Hp;
+    public int Damage;
+}
---
+++ Egg_vs_Rock/Assets/Resources/Data/GoogleSheetClass.cs
@@ -0,0 +1,27 @@
+//using System;
+//using System.Collections.Generic;
+//using UnityEngine;
+
+///// <summary>You must approach through `GoogleSheetManager.SO<GoogleSheetSO>()`</summary>
+//public class GoogleSheetSO : ScriptableObject
+//{
+//	public List<EggData> EggDataList;
+//	public List<RockData> RockDataList;
+//}
+
+//[Serializable]
+//public class EggData
+//{
+//	public string Id;
+//	public int Hp;
+//	public float Damage;
+//}
+
+//[Serializable]
+//public class RockData
+//{
+//	public string Id;
+//	public int Hp;
+//	public int Damage;
+//}
+
